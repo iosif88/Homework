e27d2c8ce31e963ca00d606e3abf0de956c5fe18
Iosif Dubikovski <iosifdubikovski@Iosifs-MacBook-Air.local>
2022-09-25 21:25:19 +0200

    Initial commit

---
+++ SquareGame1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareGame1
+//
+//  Created by Iosif Dubikovski on 9/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
